513baacc4bc51ae7cc5fbd1c040d32851c59b1de
Gonjou <ameliamarteguzman08@gmail.com>
2026-05-03 11:42:55 -0400

    Added new methods

--- tasks/storage.py
+++ tasks/storage.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-class TaskStorage:
+class TaskManager:
     def __init__(self, tasks_file="tasks.json"):
         self.tasks_file = tasks_file
 
@@ -22,13 +22,30 @@ class TaskStorage:
             json.dump(tasks, file, indent=2)
 
     def add_task(self, tasks, title, description, due_date):
-        task = {"title": title, "description": description, "due_date": due_date}
+        task = {"title": title, "description": description, "due_date": due_date, "completed": False}
         tasks.append(task)
 
-obj = TaskStorage()
+    def list_tasks(self, tasks): # format this into a table
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task['title']} - Due: {task['due_date']} - {'Completed' if task['completed'] else 'Incomplete'}")
+
+    def delete_task(self, tasks, index):
+        tasks.pop(index - 1)
+
+    def search_task(self, tasks, index):
+        print(tasks[index - 1])
+
+    def mark_as_completed(self, tasks, index):
+        tasks[index - 1]["completed"] = True
+
+
+obj = TaskManager()
 obj.load_tasks()
 tasks = []
 obj.add_task(tasks, "hello", "a greeting", "1990")
+obj.mark_as_completed(tasks, 1)
+
 obj.save_tasks(tasks)
+
 
 
